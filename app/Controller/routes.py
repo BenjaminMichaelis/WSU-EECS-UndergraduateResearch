d19316be338842e4b5a88aa3fb5ea111ab3d1258
Benjamin Michaelis <benjamin@michaelis.net>
2021-12-07 11:42:02 -0800

    Fix unhandled exception
    After submitting change to edit profile, throws unhandled exception because can't display new profile

--- app/Controller/routes.py
+++ app/Controller/routes.py
@@ -98,7 +98,7 @@ def edit_profile():
             db.session.add(current_user)
             db.session.commit()
             flash("Your changes have been saved")
-            return redirect(url_for('routes.display_profile'))
+            return redirect(url_for('routes.display_profile', user_id = current_user.id))
         pass
     elif request.method == 'GET':
         # populate the user data from DB
